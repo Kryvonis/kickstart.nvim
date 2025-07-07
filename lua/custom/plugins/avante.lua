f226c79f7d833cdc436163cb74df05b532c6a08b
Artem Kryvonis <kryvonis.artem@gmail.com>
2025-07-07 12:18:06 -0400

    Update avante with local ai

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -6,17 +6,24 @@ return {
     opts = {
       -- add any opts here
       -- for example
-      provider = 'openai',
+      -- provider = 'openai',
+      -- providers = {
+      --   openai = {
+      --     endpoint = 'https://api.openai.com/v1',
+      --     model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+      --     extra_request_body = {
+      --       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      --       temperature = 0.75,
+      --       max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      --     },
+      --   },
+      -- },
+      provider = 'ollama',
       providers = {
-        openai = {
-          endpoint = 'https://api.openai.com/v1',
-          model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
-          extra_request_body = {
-            timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-            temperature = 0.75,
-            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-          },
+        ollama = {
+          endpoint = 'http://localhost:11434',
+          model = 'devstral',
         },
       },
     },
